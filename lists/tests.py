35f2a339067b37bc5129655262673fd26e891570
lwftdr <44592483+lwftdr@users.noreply.github.com>
2020-06-15 00:02:26 +0800

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -8,5 +8,9 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New to-do did not appear in table"
+        )
                                 
 
