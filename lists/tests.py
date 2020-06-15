abfcd2cdf3e5f249bbac8f13fae00f61ccce9906
lwftdr <44592483+lwftdr@users.noreply.github.com>
2020-06-15 21:04:46 +0800

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -8,9 +8,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            "New to-do did not appear in table"
-        )
-                                
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
